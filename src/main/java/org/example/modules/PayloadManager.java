c5416dc2cf7d54a9395f7bdafe7e5e03d701da44
avijeet <avijeetjana26@gmail.com>
2024-03-12 00:59:51 +0530

    adding java aut codes

---
+++ src/main/java/org/example/modules/PayloadManager.java
@@ -0,0 +1,117 @@
+package org.example.modules;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.payloads.request.Auth;
+import org.example.payloads.request.Booking;
+import org.example.payloads.request.Bookingdates;
+import org.example.payloads.response.BookingRespons;
+import org.example.utils.FakerUtil;
+
+public class PayloadManager {
+
+    // JAVA -> JSON to that when give it to the .body()?
+
+    // Jr QA - All the payload we will keep it here
+
+
+    ObjectMapper objectMapper;
+
+    public String createPayload() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Booking booking = new Booking();
+        booking.setFirstname(FakerUtil.getUserName());
+        booking.setLastname("Dutta");
+        booking.setTotalprice(123);
+        booking.setDepositpaid(true);
+        booking.setAdditionalneeds("BreakFast");
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2022-01-01");
+        bookingdates.setCheckout("2022-01-10");
+        booking.setBookingdates(bookingdates);
+
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+
+    }
+
+    public String createPayloadnegative() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Booking booking = new Booking();
+        booking.setFirstname(FakerUtil.getUserName());
+        booking.setLastname("Dutta");
+        booking.setTotalprice(123);
+        booking.setDepositpaid(true);
+        booking.setAdditionalneeds("BreakFast");
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2022-01-01");
+        bookingdates.setCheckout("2022-01-10");
+        booking.setBookingdates(bookingdates);
+
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+
+    }
+
+    public BookingRespons JsonToObject(String jsonString) throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        BookingRespons bookingRespons = objectMapper.readValue(jsonString, BookingRespons.class);
+        return bookingRespons;
+    }
+
+    public Booking JsonToObjectPUT(String jsonString) throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Booking bookingRespons = objectMapper.readValue(jsonString, Booking.class);
+        return bookingRespons;
+    }
+
+    public String updatedPayload() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Booking booking = new Booking();
+        booking.setFirstname("Lucky");
+        booking.setLastname("Dutta");
+        booking.setTotalprice(199);
+        booking.setDepositpaid(true);
+        booking.setAdditionalneeds("Breakfast, lunch");
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2022-10-01");
+        bookingdates.setCheckout("2022-10-01");
+        booking.setBookingdates(bookingdates);
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+    }
+
+    public String updatedPayloadPatch() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Booking booking = new Booking();
+        booking.setFirstname("Lucky");
+        booking.setLastname("Dutta");
+        booking.setTotalprice(199);
+        booking.setDepositpaid(true);
+        booking.setAdditionalneeds("Breakfast, lunch");
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2022-10-01");
+        bookingdates.setCheckout("2022-10-01");
+        booking.setBookingdates(bookingdates);
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+    }
+
+    public String updatePayload(){
+        return null;
+    };
+
+
+    public String setToken() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Auth auth = new Auth();
+        auth.setUsername("admin");
+        auth.setPassword("password123");
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(auth);
+
+    }
+
+}
---
+++ src/main/java/org/example/utils/FakerUtil.java
@@ -0,0 +1,15 @@
+package org.example.utils;
+
+
+import com.github.javafaker.Faker;
+
+public class FakerUtil {
+
+    static Faker faker;
+
+    public static String getUserName(){
+        faker = new Faker();
+        String name = faker.name().fullName();
+        return name;
+    }
+}
